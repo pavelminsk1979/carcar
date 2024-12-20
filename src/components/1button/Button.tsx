94072463fba6e1ce2419d65b9cebe69a0f330f37
pavvelpotapov <pavelminsk1979@mail.ru>
2024-12-20 23:41:00 +0300

    button animation+

--- src/components/1button/Button.tsx
+++ src/components/1button/Button.tsx
@@ -3,7 +3,9 @@ import st from './Button.module.scss';
 export const Button = () => {
   return (
     <div className={st.box}>
-      <button className={st.buttonRight}>Right</button>
+      <button className={st.buttonRight} style={{ backgroundColor: '#fa7d7d' }}>
+        Right
+      </button>
       <a href="https://www.youtube.com/" className={st.buttonRight}>
         Link
       </a>
