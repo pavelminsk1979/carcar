e3b8c08effb693206abbd32561d978dc89742df5
pavvelpotapov <pavelminsk1979@mail.ru>
2024-12-24 22:17:42 +0300

    images part2

--- src/components/image/Image.tsx
+++ src/components/image/Image.tsx
@@ -1,6 +1,12 @@
 import testImage from '../../images/3388.jpg';
+import testMinSizeImage from '../../images/3399.jpg';
 import st from './Image.module.scss';
 
 export const Image = () => {
-  return <img className={st.sizeImage} src={testImage} alt="" />;
+  return (
+    <div className={st.box}>
+      <img className={st.sizeImage} src={testImage} alt="" />
+      <img className={st.sizeImage} src={testMinSizeImage} alt="" />
+    </div>
+  );
 };
