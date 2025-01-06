011dcd5922de1bd1fae3d2f80de63e2dc354ecd0
pavvelpotapov <pavelminsk1979@mail.ru>
2025-01-06 20:05:47 +0300

    size-block-with-content

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Button } from './components/1button/Button';
 import { Image } from './components/image/Image';
 import { Icon } from './components/icon/Icon';
+import { SizeBlock } from './components/size-block-for-content/SizeBlock';
 
 function App() {
   return (
     <div>
+      <SizeBlock />
       <Icon />
       <Button />
       <Image />
